7a0d44bfe0e7aa6dad22903c2211c42248625b02
Edgar Madrigal <zaiboot@gmail.com>
2015-08-12 11:07:59 -0600

    1st commit

---
+++ xUnit.Tests/xUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnit.Tests
+{
+    public class xUnitTest
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
