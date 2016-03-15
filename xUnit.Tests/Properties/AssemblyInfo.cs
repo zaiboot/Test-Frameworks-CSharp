de17cb93394292f4bc40252c2a2a794c0348acc7
zaiboot <zaiboot@gmail.com>
2016-03-14 19:38:54 -0600

    downgrading to 4.5.1

--- xUnit.Tests/Properties/AssemblyInfo.cs
+++ xUnit.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("xUnit.Tests")]
+[assembly: AssemblyTitle("xUnit.Tests.1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("xUnit.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("xUnit.Tests.1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("22927bfb-41ca-4a9c-8172-365114e19a81")]
+[assembly: Guid("d01b417f-dc7a-488e-90dc-aa59b0b16fc7")]
 
 // Version information for an assembly consists of the following four values:
 //
