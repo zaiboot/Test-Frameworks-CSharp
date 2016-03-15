4a25898fed43b253031dd55da761d908f6ad3be8
zaiboot <zaiboot@gmail.com>
2016-03-14 19:58:34 -0600

    converting to console app

---
+++ xUnit.Tests/Dummy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnit.Tests
+{
+    public class Dummy
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
