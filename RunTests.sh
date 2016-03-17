7667d9a5b4aa35b69993ba49ea52e45c63be0a33
zaiboot <zaiboot@gmail.com>
2016-03-16 20:09:33 -0600

    :Fixing sh script

--- RunTests.sh
+++ RunTests.sh
@@ -1,1 +1,2 @@
-"Packages\xunit.runner.console.2.1.0\tools\xunit.console.exe" "xUnit.Tests\bin\Debug\xUnit.Tests.dll" xUnit.Tests.json
+#mark file as executable
+mono "packages/xunit.runner.console.2.1.0/tools/xunit.console.exe" "xUnit.Tests/bin/Debug/xUnit.Tests.dll" xUnit.Tests.json
